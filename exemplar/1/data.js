4a9800a215ba75140f5b8281e78f3bf0bfcfd0d7
Tim P <contact@timpaul.co.uk>
2014-06-16 20:02:20 +0100

    Add data for exemplar 1

---
+++ exemplar/1/data.js
@@ -0,0 +1,54 @@
+var data = {
+  "service": "1. Register to vote",
+  "last-updated": "16 June 2014",
+  "userjourneys": [            
+    {
+      "title": "British citizen, recently moved house in UK",
+      "path": [{
+        "caption": "Start page",
+        "imgref": "images/1.start-page.png"
+      },{
+        "caption": "Country of residence",
+        "imgref": "images/2.country-of-residence.png"
+      },{
+        "caption": "Nationality",
+        "imgref": "images/3.nationality.png"
+      },{
+        "caption": "Date of birth",
+        "imgref": "images/4.date-of-birth.png"
+      },{
+        "caption": "Name",
+        "imgref": "images/5.name.png"
+      },{
+        "caption": "NINO",
+        "imgref": "images/6.nino.png"
+      },{
+        "caption": "Address",
+        "imgref": "images/6.2.address.png"
+      },{
+        "caption": "Second address?",
+        "imgref": "images/7.other-address.png"
+      },{
+        "caption": "Previous address?",
+        "imgref": "images/8.previous-address.png"
+      },{
+        "caption": "Enter previous address",
+        "imgref": "images/8.previous-postcode.png"
+      },{
+        "caption": "Open register?",
+        "imgref": "images/9.open-register.png"
+      },{
+        "caption": "Postal vote?",
+        "imgref": "images/10.postal-vote.png"
+      },{
+        "caption": "Contact method",
+        "imgref": "images/11.contact.png"
+      },{
+        "caption": "Confirm details",
+        "imgref": "images/12.confirmation.png"
+      },{
+        "caption": "Done",
+        "imgref": "images/13.done.png"
+      }]
+    }
+  ]}
