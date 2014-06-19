e4ac0c0758e34a94e59add6ae145ec41a27c0a06
Edward Horsford <ed.horsford@digital.cabinet-office.gov.uk>
2014-06-19 16:43:29 +0100

    Add screenshots and details for Passports

---
+++ exemplar/20/data.js
@@ -0,0 +1,102 @@
+var data = {
+  "service": "20. Passports",
+  "last-updated": "19 June 2014",
+  "userjourneys": [            
+    {
+      "title": "Photo guidance",
+      "path": [{
+        "caption": "Start page",
+        "imgref": "images/photo-guidance/1.start.png"
+      },{
+        "caption": "Photo question",
+        "imgref": "images/photo-guidance/2.photo-question.png"
+      },{
+        "caption": "Need photo",
+        "imgref": "images/photo-guidance/3.need-photo.png"
+      },{
+        "caption": "Photo guidance",
+        "imgref": "images/photo-guidance/4.photo-guidance.png"
+      }]
+    },
+    {
+      "title": "Upload a photo",
+      "path": [{
+        "caption": "Start",
+        "imgref": "images/upload-photo/1.start.png"
+      },{
+        "caption": "Photo question",
+        "imgref": "images/upload-photo/2.photo-question-expanded.png"
+      },{
+        "caption": "Email photo",
+        "imgref": "images/upload-photo/3.email.png"
+      },{
+        "caption": "Upload file",
+        "imgref": "images/upload-photo/4.upload-file.png"
+      },{
+        "caption": "Photo loading",
+        "imgref": "images/upload-photo/5.photo-loading.png"
+      }]
+    },
+    {
+      "title": "Checking photo",
+      "path": [{
+        "caption": "Photo questions introduction",
+        "imgref": "images/photo-checks/1.photo-questions.png"
+      },{
+        "caption": "Neutral expression question",
+        "imgref": "images/photo-checks/2.neutral-question.png"
+      },{
+        "caption": "Non-neutral expression",
+        "imgref": "images/photo-checks/3.neutral-fail.png"
+      },{
+        "caption": "Hat question",
+        "imgref": "images/photo-checks/4.hat-question.png"
+      },{
+        "caption": "Photo pass",
+        "imgref": "images/photo-checks/5.photo-pass.png"
+      }]
+    },
+    {
+      "title": "Passport application",
+      "path": [{
+        "caption": "IDA intro",
+        "imgref": "images/passport-application/1.ida-splash.png"
+      },{
+        "caption": "IDA login",
+        "imgref": "images/passport-application/2.ida-login.png"
+      },{
+        "caption": "Passport number",
+        "imgref": "images/passport-application/3.passport-number.png"
+      },{
+        "caption": "Confirm old passport details",
+        "imgref": "images/passport-application/4.confirm-old-details.png"
+      },{
+        "caption": "Contact details",
+        "imgref": "images/passport-application/5.contact-details.png"
+      },{
+        "caption": "Contact preferences",
+        "imgref": "images/passport-application/6.contact-preferences.png"
+      },{
+        "caption": "Choose service",
+        "imgref": "images/passport-application/7.choose-service.png"
+      },{
+        "caption": "Delivery address",
+        "imgref": "images/passport-application/8.delivery-address.png"
+      },{
+        "caption": "Passport options",
+        "imgref": "images/passport-application/9.passport-options.png"
+      },{
+        "caption": "Confirm details",
+        "imgref": "images/passport-application/10.confirm-details.png"
+      },{
+        "caption": "Declaration",
+        "imgref": "images/passport-application/11.declaration.png"
+      },{
+        "caption": "Pay",
+        "imgref": "images/passport-application/12.pay.png"
+      },{
+        "caption": "Next steps",
+        "imgref": "images/passport-application/13.next-steps.png"
+      }]
+    }
+  ]}
