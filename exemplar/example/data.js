a7da9390d272a8b81e1511f1cd0702306a530181
Tim P <contact@timpaul.co.uk>
2014-06-20 08:17:27 +0100

    Add example folder

---
+++ exemplar/example/data.js
@@ -0,0 +1,30 @@
+var data = {
+  "service": "[NAME OF EXEMPLAR]",
+  "last-updated": "[DATE HERE]",
+  "userjourneys": [            
+    {
+      "title": "[TITLE OF SCENARIO 1]",
+      "path": [{
+        "caption": "[CAPTION FOR SCREEN 1]",
+        "imgref": "images/[SCREEN 1 FILENAME]"
+      },{
+        "caption": "[CAPTION FOR SCREEN 2]",
+        "imgref": "images/[SCREEN 2 FILENAME]"
+      },{
+        "caption": "[CAPTION FOR SCREEN 2]",
+        "imgref": "images/[SCREEN 2 FILENAME]"
+      }]
+    },{
+      "title": "[TITLE OF SCENARIO 2]",
+      "path": [{
+        "caption": "[CAPTION FOR SCREEN 1]",
+        "imgref": "images/[SCREEN 1 FILENAME]"
+      },{
+        "caption": "[CAPTION FOR SCREEN 2]",
+        "imgref": "images/[SCREEN 2 FILENAME]"
+      },{
+        "caption": "[CAPTION FOR SCREEN 2]",
+        "imgref": "images/[SCREEN 2 FILENAME]"
+      }]
+    }
+  ]}
