dc8aa8b09b3be3889cbd5b303129c5a873b609a2
Tim P <contact@timpaul.co.uk>
2014-06-27 16:11:14 +0100

    Add 1st scenario for exemplar 7

--- exemplar/7/data.js
+++ exemplar/7/data.js
@@ -1,35 +1,58 @@
 var data = {
   "service": "7. Waste carrier registration",
-  "last-updated": "[DATE HERE]",
-  "userjourneys": [            
+  "last-updated": "27 June 2014",
+  "userjourneys": [
     {
-      "title": "[TITLE OF SCENARIO 1]",
+      "title": "Sole trader, deals mainly with other's waste",
       "path": [{
-        "caption": "[CAPTION FOR SCREEN 1]",
-        "imgref": "images/[SCREEN 1 FILENAME]",
-        "note": [
-          "This is a note.",
-          "This is another note."
-        ]
+        "caption": "Start page",
+        "imgref": "images/1.start-page.png",
+        "note": "User clicks 'Get started'"
       },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]",
-        "note": "This is a note."
+        "caption": "Business type",
+        "imgref": "images/2.business-type.png",
+        "note": "User selects 'Sole trader'."
       },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]"
-      }]
-    },{
-      "title": "[TITLE OF SCENARIO 2]",
-      "path": [{
-        "caption": "[CAPTION FOR SCREEN 1]",
-        "imgref": "images/[SCREEN 1 FILENAME]"
+        "caption": "Waste from other businesses?",
+        "imgref": "images/3.waste-from-other-businesses.png",
+        "note": "User selects 'Yes'."
       },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]"
+        "caption": "Kind of service",
+        "imgref": "images/4.kind-of-service.png",
+        "note": "User selects 'Waste disposal is the main service we provide'."
       },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]"
+        "caption": "Kind of waste",
+        "imgref": "images/5.kind-of-waste.png",
+        "note": "User selects 'Yes'."
+      },{
+        "caption": "Address",
+        "imgref": "images/6.address.gif",
+        "note": "User enters details.",
+        "tags": ['address-lookup']
+      },{
+        "caption": "Contact details",
+        "imgref": "images/7.contact-details.png",
+        "note": "User enters contact details."
+      },{
+        "caption": "Check details",
+        "imgref": "images/8.check-details.png",
+        "note": "User checks details and makes declaration.",
+        "tags": ['summary-page', 'declaration']
+      },{
+        "caption": "Create account",
+        "imgref": "images/9.create-account.png",
+        "note": "User adds account details so they can re-edit their registration data.",
+        "tags": ['create-account']
+      },{
+        "caption": "Confirm account",
+        "imgref": "images/10.confirm-account.png",
+        "note": "User waits for email to appear in inbox.",
+        "tags": ['confirm-account']
+      },{
+        "caption": "Confirmation Email",
+        "imgref": "images/confirmation-email.png",
+        "note": "User receives confirmaton email.",
+        "tags": ['confirm-account', 'email']
       }]
     }
   ]}
