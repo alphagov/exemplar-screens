77e40536873d4064eb34994d653781f01f3e006c
Tim P <contact@timpaul.co.uk>
2014-07-03 08:45:53 +0100

    Draft tag index and dummy data-structure

---
+++ exemplar/tags/data.js
@@ -0,0 +1,73 @@
+var data = {
+  "service": "Tags",
+  "last-updated": "3 July 2014",
+  "tags": [
+    {
+      "title": "address-lookup",
+      "images": [{
+        "exemplar_number": 7,
+        "exemplar_name": "Waste carrier registration",
+        "caption": "Address",
+        "imgref": "images/6.address.gif",
+        "note": "User enters details.",
+        "tags": ['address-lookup']
+      }]
+    },{
+      "title": "summary-page",
+      "images": [{
+        "exemplar_number": 7,
+        "exemplar_name": "Waste carrier registration",
+        "caption": "Check details",
+        "imgref": "images/8.check-details.png",
+        "note": "User checks details and makes declaration.",
+        "tags": ['summary-page', 'declaration']
+      }]
+    },{
+      "title": "declaration",
+      "images": [{
+        "exemplar_number": 7,
+        "exemplar_name": "Waste carrier registration",
+        "caption": "Check details",
+        "imgref": "images/8.check-details.png",
+        "note": "User checks details and makes declaration.",
+        "tags": ['summary-page', 'declaration']
+      }]
+    },{
+      "title": "create-account",
+      "images": [{
+        "exemplar_number": 7,
+        "exemplar_name": "Waste carrier registration",
+        "caption": "Create account",
+        "imgref": "images/9.create-account.png",
+        "note": "User adds account details so they can re-edit their registration data.",
+        "tags": ['create-account']
+      }]
+    },{
+      "title": "confirm-account",
+      "images": [{
+        "exemplar_number": 7,
+        "exemplar_name": "Waste carrier registration",
+        "caption": "Confirm account",
+        "imgref": "images/10.confirm-account.png",
+        "note": "User waits for email to appear in inbox.",
+        "tags": ['confirm-account']
+      },{
+        "exemplar_number": 7,
+        "exemplar_name": "Waste carrier registration",
+        "caption": "Confirmation Email",
+        "imgref": "images/confirmation-email.png",
+        "note": "User receives confirmaton email.",
+        "tags": ['confirm-account', 'email']
+      }]
+    },{
+      "title": "email",
+      "images": [{
+        "exemplar_number": 7,
+        "exemplar_name": "Waste carrier registration",
+        "caption": "Confirmation Email",
+        "imgref": "images/confirmation-email.png",
+        "note": "User receives confirmaton email.",
+        "tags": ['confirm-account', 'email']
+      }]
+    }
+  ]}
