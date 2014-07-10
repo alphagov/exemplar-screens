a1b46f4d1982356756cce24466f78e0b3a52a69d
Tim P <contact@timpaul.co.uk>
2014-07-10 21:59:22 +0100

    Add CAP prototypes folder

---
+++ exemplar/8.2/data.js
@@ -0,0 +1,63 @@
+var data = {
+  "service": "8. Rural support - Prototypes",
+  "last-updated": "10 July",
+  "userjourneys": [ {
+      "title": "Provide information about a land parcel",
+      "path": [{
+        "caption": "Land page",
+        "imgref": "images/IMG_0937.jpg",
+        "note" : [
+          "The user is viewing land parcels for Primrose Farm.",
+          "They've already given details for the first one, which is why it's marked in green on the list and map.",
+          "They've moused over parcel 5 on the map 'Alfafa field'. The list item is also highlighted.",
+          "They click on the parcel."
+        ]
+      },{
+        "caption": "Parcel page",
+        "imgref": "images/IMG_0938.jpg",
+        "note" : [
+          "Now the map is zoomed in on the parcel.",
+          "The user can change the name of the parcel by clicking the edit button next to it.",
+          "There's a table below showing the information they need to provide about this land.",
+          "The user clicks the first item."
+        ]
+      },{
+        "caption": "Land use",
+        "imgref": "images/IMG_0939.jpg",
+        "note" : [
+          "The user is asked what this land is used for.",
+          "As they type into the field, matching items from the full list are shown below.",
+          "The user can select one or more of these items.",
+          "They do this and click 'Next'."
+        ]
+      },{
+        "caption": "Buffer strip",
+        "imgref": "images/IMG_0941.jpg",
+        "note" : [
+          "The user is asked if this land has a buffer strip.",
+          "They answer yes and are then asked to set the width of the strip, either with a slider or a text field.",
+          "As the value changes the map updates, showing the outline of the strip.",
+          "They set the width and click 'Next'."
+        ]
+      },{
+        "caption": "Adjust buffer strip",
+        "imgref": "images/IMG_0942.jpg",
+        "note" : [
+          "If they want to the user can adjust the buffer strip by dragging the lines on the map.",
+          "If they get stuck they can click 'reset' to undo any adjustments.",
+          "If they really struggle they can click 'ask for help'. They'll be able to provide more information and then a job will be created for a digitiser to take a look.",
+          "When they're done, they click 'Next'."
+        ]
+      },{
+        "caption": "Hedges",
+        "imgref": "images/IMG_0940.jpg",
+        "note" : [
+          "The user is asked if this land has any hedges.",
+          "They answer 'Yes' and are instructed to draw the hedges on the map.",
+          "They mark the start and end points of hedges. If both points are on a parcel boundary the hedge follows the boundary, otherwise it's a straight line.",
+          "As they mark the hedges, the length of the hedge is shown next to the cursor",
+          "When they're done, they click 'Next'"
+        ]
+      }]
+    }
+  ]}
