ab39b4bdf9c67d88d0d89023f558e9af852262eb
Tim P <contact@timpaul.co.uk>
2014-07-10 20:50:50 +0100

    1st pass at scenarios

--- exemplar/8/data.js
+++ exemplar/8/data.js
@@ -2,7 +2,28 @@ var data = {
   "service": "8. Rural support (Common Agricultural Policy)",
   "last-updated": "10 July",
   "userjourneys": [ {
-      "title": "User adds a feature to a land parcel",
+      "title": "First time access",
+      "path": [{
+        "caption": "Home page",
+        "imgref": "images/INitial page  CAP Information Service.png"
+      },{
+        "caption": "Identity assurance"
+      },{
+        "caption": "Terms and conditions",
+        "imgref": "images/Terms and conditions NewReg1  CAP Information Service.png"
+      },{
+        "caption": "Terms and conditions",
+        "imgref": "images/Terms and conditions   CAP Information Service.png"
+      },{
+        "caption": "Check your personal details",
+        "imgref": "images/New customer registration NewReg2  CAP Information Service.png"
+      },{
+        "caption": "Done",
+        "imgref": "images/My Account NewRegComplete  CAP Information Service.png"
+      }]
+
+    },{
+      "title": "Add a feature to a land parcel",
       "path": [{
         "caption": "Start date",
         "imgref": "images/Add a feature   CAP Information Service.png",
@@ -28,127 +49,198 @@ var data = {
         "imgref": "images/Land change submitted 6  CAP Information Service.png"
       }]
     },{
-      "title": "Other screens",
+      "title": "Add a new business",
       "path": [{
-        "caption": "Add new business",
-        "imgref": "images/Add new business   CAP Information Service.png"
+        "caption": "My account",
+        "imgref": "images/My Account   CAP Information Service.png",
+        "notes": "User selects 'Add a new business"
       },{
-        "caption": "Add new business 2",
-        "imgref": "images/Add new business 2  before you start   CAP Information Service.png"
+        "caption": "Business type",
+        "imgref": "images/Add new business   CAP Information Service.png",
+        "note": "User specifies business type. In this case an agricultural business."
       },{
-        "caption": "Additional questions 4",
-        "imgref": "images/Additional questions 4  CAP Information Service.png"
+        "caption": "Before you start",
+        "imgref": "images/Add new business 2  before you start   CAP Information Service.png",
+        "note": "User is told what information they'll need to provide and what they'll need to do."
       },{
-        "caption": "Amend personal details",
-        "imgref": "images/Amend personal details   CAP Information Service.png"
+        "caption": "Business details",
+        "imgref": "images/Create business 3  CAP Information Service.png",
+        "note": "User enters business details"
       },{
-        "caption": "Apply for new entitlements",
-        "imgref": "images/Apply for new entitlements   CAP Information Service.png"
-      },{
-        "caption": "Business created",
+        "caption": "Done",
         "imgref": "images/Business created   CAP Information Service.png"
-      },{
-        "caption": "Business overview ",
-        "imgref": "images/Business overview   CAP Information Service.png"
-      },{
-        "caption": "Check business details",
-        "imgref": "images/Check business details   CAP Information Service.png"
-      },{
-        "caption": "Check your personal details",
-        "imgref": "images/Check your personal details   CAP Information Service.png"
-      },{
-        "caption": "Create business 3",
-        "imgref": "images/Create business 3  CAP Information Service.png"
-      },{
-        "caption": "Entitlement query",
-        "imgref": "images/Entitlement query   CAP Information Service.png"
-      },{
-        "caption": "Entitlement query submitted",
-        "imgref": "images/Entitlement query submitted   CAP Information Service.png"
-      },{
-        "caption": "Entitlements",
-        "imgref": "images/Entitlements   CAP Information Service.png"
-      },{
-        "caption": "Entitlements Transfer",
-        "imgref": "images/Entitlements Transfer  CAP Information Service.png"
-      },{
-        "caption": "Firm of agents",
-        "imgref": "images/Firm of agents  CAP Information Service.png"
-      },{
-        "caption": "Give people permissions",
-        "imgref": "images/Give people permissions   CAP Information Service.png"
-      },{
-        "caption": "Give people permissions 2",
-        "imgref": "images/Give people permissions 2  CAP Information Service.png"
-      },{
-        "caption": "Give people permissions 3",
-        "imgref": "images/Give people permissions 3  CAP Information Service.png"
-      },{
-        "caption": "Give people permissions 4",
-        "imgref": "images/Give people permissions 4  CAP Information Service.png"
-      },{
-        "caption": "Give people permissions Firm",
-        "imgref": "images/Give people permissions Firm  CAP Information Service.png"
-      },{
-        "caption": "Give people permissions Firm 2",
-        "imgref": "images/Give people permissions Firm 2  CAP Information Service.png"
-      },{
-        "caption": "Give people permissions Firm 3",
-        "imgref": "images/Give people permissions Firm 3  CAP Information Service.png"
-      },{
-        "caption": "Give people permissions Firm 4",
-        "imgref": "images/Give people permissions Firm 4  CAP Information Service.png"
-      },{
-        "caption": "Give people permissions Firm 5",
-        "imgref": "images/Give people permissions Firm 5  CAP Information Service.png"
-      },{
-        "caption": "INitial page",
-        "imgref": "images/INitial page  CAP Information Service.png"
-      },{
-        "caption": "Land for business",
-        "imgref": "images/Land for business   CAP Information Service.png"
-      },{
-        "caption": "Land Map for business",
-        "imgref": "images/Land Map for business   CAP Information Service.png"
-      },{
-        "caption": "Land Table for business",
-        "imgref": "images/Land Table for business   CAP Information Service.png"
-      },{
-        "caption": "Manage land",
-        "imgref": "images/Manage land   CAP Information Service.png"
-      },{
-        "caption": "Manage land Finish",
-        "imgref": "images/Manage land Finish  CAP Information Service.png"
-      },{
-        "caption": "Manage land Organic",
-        "imgref": "images/Manage land Organic  CAP Information Service.png"
-      },{
-        "caption": "My Account",
-        "imgref": "images/My Account   CAP Information Service.png"
       },{
         "caption": "My Account Bizadded",
         "imgref": "images/My Account Bizadded  CAP Information Service.png"
+      }]
+    },{
+      "title": "Add a new firm of agents",
+      "path": [{
+        "caption": "My account",
+        "imgref": "images/My Account   CAP Information Service.png",
+        "notes": "User selects 'Add a new business"
       },{
-        "caption": "My Account NewRegComplete",
-        "imgref": "images/My Account NewRegComplete  CAP Information Service.png"
+        "caption": "Business type",
+        "imgref": "images/Add new business   CAP Information Service.png",
+        "note": "User specifies business type. In this case a firm of agents."
       },{
-        "caption": "New customer registration NewReg2",
-        "imgref": "images/New customer registration NewReg2  CAP Information Service.png"
+        "caption": "Firm of agents",
+        "imgref": "images/Firm of agents  CAP Information Service.png"
+      }]
+    },{
+      "title": "Check business details",
+      "path": [{
+        "caption": "My account",
+        "imgref": "images/My Account   CAP Information Service.png",
+        "note": "User clicks 'Check business"
+      },{
+        "caption": "Check business details",
+        "imgref": "images/Check business details   CAP Information Service.png",
+        "note": "User checks and confirms business details"
+      }]
+    },{
+      "title": "View and amend personal details",
+      "path": [{
+        "caption": "My account",
+        "imgref": "images/My Account   CAP Information Service.png",
+        "note": "User clicks 'View and amend personal details"
+      },{
+        "caption": "Amend personal details",
+        "imgref": "images/Amend personal details   CAP Information Service.png"
+      }]
+    },{
+      "title": "Query an entitlement",
+      "path": [{
+        "caption": "Entitlements page",
+        "imgref": "images/Entitlements   CAP Information Service.png",
+        "note": "User selects 'Submit a query about your entitlements'."
+      },{
+        "caption": "Enter query",
+        "imgref": "images/Entitlement query   CAP Information Service.png",
+        "note": "Use selects a category for the enquiry and gives details."
+      },{
+        "caption": "Done",
+        "imgref": "images/Entitlement query submitted   CAP Information Service.png"
+      }]
+    },{
+      "title": "Transfer an entitlement",
+      "path": [{
+        "caption": "Entitlements page",
+        "imgref": "images/Entitlements   CAP Information Service.png",
+        "note": "User selects 'Transfer entitlements'."
+      },{
+        "caption": "Entitlements Transfer",
+        "imgref": "images/Entitlements Transfer  CAP Information Service.png"
+      }]
+    },{
+      "title": "Apply for new entitlements",
+      "path": [{
+        "caption": "Entitlements page",
+        "imgref": "images/Entitlements   CAP Information Service.png",
+        "note": "User selects 'Apply for new entitlements'."
+      },{
+        "caption": "Entitlements Transfer",
+        "imgref": "images/Apply for new entitlements   CAP Information Service.png"
+      }]
+    },{
+      "title": "Give a person permission to manage your account",
+      "path": [{
+        "caption": "Business overview ",
+        "imgref": "images/Business overview   CAP Information Service.png",
+        "note": "User clicks 'View people and permissions'."
       },{
         "caption": "People and permissions",
-        "imgref": "images/People and permissions   CAP Information Service.png"
+        "imgref": "images/People and permissions   CAP Information Service.png",
+        "note": "User clicks 'Find a person'"
       },{
-        "caption": "Permissions updated",
+        "caption": "Find person",
+        "imgref": "images/Give people permissions   CAP Information Service.png"
+      },{
+        "caption": "Business relationship",
+        "imgref": "images/Give people permissions 2  CAP Information Service.png"
+      },{
+        "caption": "Set permissions",
+        "imgref": "images/Give people permissions 3  CAP Information Service.png"
+      },{
+        "caption": "Summary",
+        "imgref": "images/Give people permissions 4  CAP Information Service.png"
+      },{
+        "caption": "Done",
         "imgref": "images/Permissions updated   CAP Information Service.png"
+      }]
+    },{
+      "title": "Give a firm permission to manage your account",
+      "path": [{
+        "caption": "Business overview ",
+        "imgref": "images/Business overview   CAP Information Service.png",
+        "note": "User clicks 'View people and permissions'."
       },{
+        "caption": "People and permissions",
+        "imgref": "images/People and permissions   CAP Information Service.png",
+        "note": "User clicks 'Find a firm'"
+      },{
+        "caption": "Find firm",
+        "imgref": "images/Give people permissions Firm  CAP Information Service.png"
+      },{
+        "caption": "Add people",
+        "imgref": "images/Give people permissions Firm 2  CAP Information Service.png"
+      },{
+        "caption": "Set permissions",
+        "imgref": "images/Give people permissions Firm 3  CAP Information Service.png"
+      },{
+        "caption": "Summary",
+        "imgref": "images/Give people permissions Firm 4  CAP Information Service.png"
+      },{
+        "caption": "Done",
+        "imgref": "images/Give people permissions Firm 5  CAP Information Service.png"
+      }]
+    },{
+      "title": "Check land parcels",
+      "path": [{
+        "caption": "Land page",
+        "imgref": "images/Land for business   CAP Information Service.png",
+        "note": "User clicks 'Go to map view'."
+      },{
+        "caption": "Map view",
+        "imgref": "images/Land Map for business   CAP Information Service.png",
+        "note": "User clicks 'Table' tab."
+      },{
+        "caption": "Table view",
+        "imgref": "images/Land Table for business   CAP Information Service.png"
+      }]
+    },{
+      "title": "User changes organic status of a land parcel",
+      "path": [{
+        "caption": "Land page",
+        "imgref": "images/Land for business   CAP Information Service.png",
+        "note": "User clicks 'Go to map view'."
+      },{
+        "caption": "Map view",
+        "imgref": "images/Land Map for business   CAP Information Service.png",
+        "note": "User clicks on a parcel."
+      },{
+        "caption": "Parcel view",
+        "imgref": "images/Manage land   CAP Information Service.png",
+        "note" : "User clicks modify organic status"
+      },{
+        "caption": "Organic status",
+        "imgref": "images/Manage land Organic  CAP Information Service.png",
+        "note": "User modifies organic status"
+      },{
+        "caption": "Done",
+        "imgref": "images/Manage land Finish  CAP Information Service.png"
+      }]
+    },{
+      "title": "Scheme eligibility",
+      "path": [{
         "caption": "Scheme eligibility",
         "imgref": "images/Scheme eligibility   CAP Information Service.png"
-      },{
-        "caption": "Terms and conditions",
-        "imgref": "images/Terms and conditions   CAP Information Service.png"
-      },{
-        "caption": "Terms and conditions NewReg1",
-        "imgref": "images/Terms and conditions NewReg1  CAP Information Service.png"
+      }]
+    },{
+      "title": "Other screens",
+      "path": [{
+        "caption": "Check your personal details",
+        "imgref": "images/Check your personal details   CAP Information Service.png"
       }]
     }
   ]}
