f54cd1de4989a72729ce007ded11fc30011484c7
Tim P <contact@timpaul.co.uk>
2014-07-10 17:47:38 +0100

    Add all screens and show in alphabetical order

--- exemplar/8/data.js
+++ exemplar/8/data.js
@@ -1,35 +1,144 @@
 var data = {
   "service": "8. Rural support (Common Agricultural Policy)",
-  "last-updated": "[DATE HERE]",
-  "userjourneys": [            
+  "last-updated": "10 July",
+  "userjourneys": [
     {
-      "title": "[TITLE OF SCENARIO 1]",
+      "title": "All screens - alphabetical",
       "path": [{
-        "caption": "[CAPTION FOR SCREEN 1]",
-        "imgref": "images/[SCREEN 1 FILENAME]",
-        "note": [
-          "This is a note.",
-          "This is another note."
-        ]
+        "caption": "Add a feature",
+        "imgref": "images/Add a feature   CAP Information Service.png"
       },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]",
-        "note": "This is a note."
+        "caption": "Add a feature 2",
+        "imgref": "images/Add a feature 2  CAP Information Service.png"
       },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]"
-      }]
-    },{
-      "title": "[TITLE OF SCENARIO 2]",
-      "path": [{
-        "caption": "[CAPTION FOR SCREEN 1]",
-        "imgref": "images/[SCREEN 1 FILENAME]"
+        "caption": "Add new business",
+        "imgref": "images/Add new business   CAP Information Service.png"
       },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]"
+        "caption": "Add new business 2",
+        "imgref": "images/Add new business 2  before you start   CAP Information Service.png"
       },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]"
+        "caption": "Add the Pond 3",
+        "imgref": "images/Add the Pond 3  CAP Information Service.png"
+      },{
+        "caption": "Additional questions 4",
+        "imgref": "images/Additional questions 4  CAP Information Service.png"
+      },{
+        "caption": "Amend personal details",
+        "imgref": "images/Amend personal details   CAP Information Service.png"
+      },{
+        "caption": "Apply for new entitlements",
+        "imgref": "images/Apply for new entitlements   CAP Information Service.png"
+      },{
+        "caption": "Business created",
+        "imgref": "images/Business created   CAP Information Service.png"
+      },{
+        "caption": "Business overview ",
+        "imgref": "images/Business overview   CAP Information Service.png"
+      },{
+        "caption": "Check business details",
+        "imgref": "images/Check business details   CAP Information Service.png"
+      },{
+        "caption": "Check your changes 5",
+        "imgref": "images/Check your changes 5  CAP Information Service.png"
+      },{
+        "caption": "Check your personal details",
+        "imgref": "images/Check your personal details   CAP Information Service.png"
+      },{
+        "caption": "Create business 3",
+        "imgref": "images/Create business 3  CAP Information Service.png"
+      },{
+        "caption": "Entitlement query",
+        "imgref": "images/Entitlement query   CAP Information Service.png"
+      },{
+        "caption": "Entitlement query submitted",
+        "imgref": "images/Entitlement query submitted   CAP Information Service.png"
+      },{
+        "caption": "Entitlements",
+        "imgref": "images/Entitlements   CAP Information Service.png"
+      },{
+        "caption": "Entitlements Transfer",
+        "imgref": "images/Entitlements Transfer  CAP Information Service.png"
+      },{
+        "caption": "Firm of agents",
+        "imgref": "images/Firm of agents  CAP Information Service.png"
+      },{
+        "caption": "Give people permissions",
+        "imgref": "images/Give people permissions   CAP Information Service.png"
+      },{
+        "caption": "Give people permissions 2",
+        "imgref": "images/Give people permissions 2  CAP Information Service.png"
+      },{
+        "caption": "Give people permissions 3",
+        "imgref": "images/Give people permissions 3  CAP Information Service.png"
+      },{
+        "caption": "Give people permissions 4",
+        "imgref": "images/Give people permissions 4  CAP Information Service.png"
+      },{
+        "caption": "Give people permissions Firm",
+        "imgref": "images/Give people permissions Firm  CAP Information Service.png"
+      },{
+        "caption": "Give people permissions Firm 2",
+        "imgref": "images/Give people permissions Firm 2  CAP Information Service.png"
+      },{
+        "caption": "Give people permissions Firm 3",
+        "imgref": "images/Give people permissions Firm 3  CAP Information Service.png"
+      },{
+        "caption": "Give people permissions Firm 4",
+        "imgref": "images/Give people permissions Firm 4  CAP Information Service.png"
+      },{
+        "caption": "Give people permissions Firm 5",
+        "imgref": "images/Give people permissions Firm 5  CAP Information Service.png"
+      },{
+        "caption": "INitial page",
+        "imgref": "images/INitial page  CAP Information Service.png"
+      },{
+        "caption": "Land change submitted 6",
+        "imgref": "images/Land change submitted 6  CAP Information Service.png"
+      },{
+        "caption": "Land for business",
+        "imgref": "images/Land for business   CAP Information Service.png"
+      },{
+        "caption": "Land Map for business",
+        "imgref": "images/Land Map for business   CAP Information Service.png"
+      },{
+        "caption": "Land Table for business",
+        "imgref": "images/Land Table for business   CAP Information Service.png"
+      },{
+        "caption": "Manage land",
+        "imgref": "images/Manage land   CAP Information Service.png"
+      },{
+        "caption": "Manage land Finish",
+        "imgref": "images/Manage land Finish  CAP Information Service.png"
+      },{
+        "caption": "Manage land Organic",
+        "imgref": "images/Manage land Organic  CAP Information Service.png"
+      },{
+        "caption": "My Account",
+        "imgref": "images/My Account   CAP Information Service.png"
+      },{
+        "caption": "My Account Bizadded",
+        "imgref": "images/My Account Bizadded  CAP Information Service.png"
+      },{
+        "caption": "My Account NewRegComplete",
+        "imgref": "images/My Account NewRegComplete  CAP Information Service.png"
+      },{
+        "caption": "New customer registration NewReg2",
+        "imgref": "images/New customer registration NewReg2  CAP Information Service.png"
+      },{
+        "caption": "People and permissions",
+        "imgref": "images/People and permissions   CAP Information Service.png"
+      },{
+        "caption": "Permissions updated",
+        "imgref": "images/Permissions updated   CAP Information Service.png"
+      },{
+        "caption": "Scheme eligibility",
+        "imgref": "images/Scheme eligibility   CAP Information Service.png"
+      },{
+        "caption": "Terms and conditions",
+        "imgref": "images/Terms and conditions   CAP Information Service.png"
+      },{
+        "caption": "Terms and conditions NewReg1",
+        "imgref": "images/Terms and conditions NewReg1  CAP Information Service.png"
       }]
     }
   ]}
