fa602c0045109deabacdeb79194f0e4858bcbd12
Alex Torrance <alex.torrance@digital.cabinet-office.gov.uk>
2014-08-05 11:48:57 +0100

    Add HMRC exemplar prototype screens

--- exemplar/17/data.js
+++ exemplar/17/data.js
@@ -1,35 +1,34 @@
 var data = {
   "service": "17. Your tax account",
-  "last-updated": "[DATE HERE]",
-  "userjourneys": [            
-    {
-      "title": "[TITLE OF SCENARIO 1]",
-      "path": [{
-        "caption": "[CAPTION FOR SCREEN 1]",
-        "imgref": "images/[SCREEN 1 FILENAME]",
-        "note": [
-          "This is a note.",
-          "This is another note."
-        ]
-      },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]",
-        "note": "This is a note."
-      },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]"
-      }]
+  "last-updated": "1 August 2014",
+  "userjourneys": [
+  {
+    "title": "Tax account prototype (30 July 2014)",
+    "path": [{
+      "caption": "Opt in screen",
+      "imgref": "images/prototype-2014-07-30/01-self-assessment-opt-in.png",
     },{
-      "title": "[TITLE OF SCENARIO 2]",
-      "path": [{
-        "caption": "[CAPTION FOR SCREEN 1]",
-        "imgref": "images/[SCREEN 1 FILENAME]"
-      },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]"
-      },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]"
-      }]
-    }
-  ]}
+      "caption": "Opt in screen",
+      "imgref": "images/prototype-2014-07-30/01a-self-assessment-opt-in.png",
+      "note": "Email address entry revealed."
+    },{
+      "caption": "To do list",
+      "imgref": "images/prototype-2014-07-30/02-todo-list.png"
+    },{
+      "caption": "Your taxes overview",
+      "imgref": "images/prototype-2014-07-30/03-your-taxes.png"
+    },{
+      "caption": "Self Assessment detail",
+      "imgref": "images/prototype-2014-07-30/03a-self-assessment.png"
+    },{
+      "caption": "Messages inbox",
+      "imgref": "images/prototype-2014-07-30/04-messages-inbox.png"
+    },{
+      "caption": "Message detail",
+      "imgref": "images/prototype-2014-07-30/04a-message-detail.png"
+    },{
+      "caption": "Your details",
+      "imgref": "images/prototype-2014-07-30/05-your-details.png"
+    }]
+  }]
+}
