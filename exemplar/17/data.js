261bf237790fbf9869c70d77165b18b087219307
Alex Torrance <alex.torrance@digital.cabinet-office.gov.uk>
2014-08-05 12:04:47 +0100

    Add older HMRC prototype images

--- exemplar/17/data.js
+++ exemplar/17/data.js
@@ -6,11 +6,11 @@ var data = {
     "title": "Tax account prototype (30 July 2014)",
     "path": [{
       "caption": "Opt in screen",
-      "imgref": "images/prototype-2014-07-30/01-self-assessment-opt-in.png",
+      "imgref": "images/prototype-2014-07-30/01-self-assessment-opt-in.png"
     },{
       "caption": "Opt in screen",
       "imgref": "images/prototype-2014-07-30/01a-self-assessment-opt-in.png",
-      "note": "Email address entry revealed."
+      "note": "Email address entry revealed"
     },{
       "caption": "To do list",
       "imgref": "images/prototype-2014-07-30/02-todo-list.png"
@@ -29,6 +29,52 @@ var data = {
     },{
       "caption": "Your details",
       "imgref": "images/prototype-2014-07-30/05-your-details.png"
+    }]
+  },{
+    "title": "Tax account prototype",
+    "path": [{
+      "caption": "Account overview",
+      "imgref": "images/prototype/0.account-overview.png"
+    },{
+      "caption": "Messages inbox",
+      "imgref": "images/prototype/1.messages.png"
+    },{
+      "caption": "Message detail",
+      "imgref": "images/prototype/2.message-pay.png"
+    },{
+      "caption": "Sign out",
+      "imgref": "images/prototype/3.sign-out.png"
+    },{
+      "caption": "To do",
+      "imgref": "images/prototype/4.to-do.png"
+    },{
+      "caption": "Your taxes",
+      "imgref": "images/prototype/5.your-taxes.png"
+    },{
+      "caption": "Messages",
+      "imgref": "images/prototype/6.messages.png"
+    },{
+      "caption": "Important dates",
+      "imgref": "images/prototype/7.dates.png"
+    },{
+      "caption": "Account overview",
+      "imgref": "images/prototype/8.returns-up-to-date.png",
+      "note": "Self Assessment returns up to date"
+    },{
+      "caption": "Sign in",
+      "imgref": "images/prototype/9.sign-in.png"
+    },{
+      "caption": "Pay Self Assessment",
+      "imgref": "images/prototype/10.pay.png"
+    },{
+      "caption": "Pay by Direct Debit",
+      "imgref": "images/prototype/11.pay-direct-debit.png"
+    },{
+      "caption": "Pay by credit or debit card",
+      "imgref": "images/prototype/12.credit-debit-card.png"
+    },{
+      "caption": "By by bank transfer",
+      "imgref": "images/prototype/13.bank-account-transfer.png"
     }]
   }]
 }
