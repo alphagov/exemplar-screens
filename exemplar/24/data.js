e29fe30b6d4826e37828427f04d59a570319f740
Mat Harden <matharden@gmail.com>
2014-11-27 18:41:52 +0000

    Add "Visit request" journey for Visit someone in prison

--- exemplar/24/data.js
+++ exemplar/24/data.js
@@ -1,35 +1,45 @@
 var data = {
-  "service": "24. Prison visit booking",
-  "last-updated": "[DATE HERE]",
-  "userjourneys": [            
+  "service": "24. Visit someone in prison (formally Prison visit booking)", // service title
+  "last-updated": "24 November 2014", // date last updated
+  "userjourneys": [
     {
-      "title": "[TITLE OF SCENARIO 1]",
-      "path": [{
-        "caption": "[CAPTION FOR SCREEN 1]",
-        "imgref": "images/[SCREEN 1 FILENAME]",
-        "note": [
-          "This is a note.",
-          "This is another note."
-        ]
-      },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]",
-        "note": "This is a note."
-      },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]"
-      }]
-    },{
-      "title": "[TITLE OF SCENARIO 2]",
-      "path": [{
-        "caption": "[CAPTION FOR SCREEN 1]",
-        "imgref": "images/[SCREEN 1 FILENAME]"
-      },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]"
-      },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]"
-      }]
+      "title": "Booking request", // user journey title
+      "path": [ // array of images
+        {
+          "caption": "Start page", // image caption
+          "imgref": "images/0.start_page.png", // image path
+          "note": "This process enables the public to request social visits online. Prison booking staff receive an email and confirm via a form which generates an email response."
+        },
+        {
+          "caption": "Prisoner details",
+          "imgref": "images/1.prisoner.png",
+
+        },
+        {
+          "caption": "Visitor details",
+          "imgref": "images/2.visitor.png"
+        },
+        {
+          "caption": "Additional visitors",
+          "imgref": "images/3.visitor_b.png"
+        },
+        {
+          "caption": "Choose a visiting slot",
+          "imgref": "images/4.slots.png",
+          "note": "We encourage visitors to choose up to 3 slots, in case their first choice is not available."
+        },
+        {
+          "caption": "Choose a visiting slot - chosen states",
+          "imgref": "images/5.slots_b.png"
+        },
+        {
+          "caption": "Check your request",
+          "imgref": "images/6.summary.png"
+        },
+        {
+          "caption": "Request sent",
+          "imgref": "images/7.request_sent.png"
+        }
+      ]
     }
   ]}
