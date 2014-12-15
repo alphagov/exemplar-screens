c0839a4bf844ac92dde1b551fc43204a3079f310
Tim P <contact@timpaul.co.uk>
2014-12-15 15:57:10 +0000

    2nd draft

--- exemplar/8/data.js
+++ exemplar/8/data.js
@@ -4,36 +4,157 @@ var data = {
   "userjourneys": [ {
       "title": "Registration",
       "path": [{
+        "caption": "Start page",
+        "imgref": "images/start-page.png"
+      },{
         "caption": "Home page",
-        "imgref": "images/1.home-page.png"
+        "imgref": "images/home-page.png"
       },{
         "caption": "Email",
         "imgref": "images/create-account-email.png"
       },{
         "caption": "Create account",
-        "imgref": "images/2.register-phone.png",
+        "imgref": "images/register-phone.png",
         "note": "User enters registration code and creates a password"
       },{
+        "caption": "Validation errors",
+        "imgref": "images/register-phone-errors.png",
+        "note": "Validation errors"
+      },{
         "caption": "Your businesses",
-        "imgref": "images/3.your-businesses.png",
+        "imgref": "images/your-businesses.png",
         "note": "User sees list of businesses they have access to"
       },{
         "caption": "Messages",
-        "imgref": "images/4.messages.png",
+        "imgref": "images/messages.png",
         "note": "User views messages"
       },{
+        "caption": "Business page",
+        "imgref": "images/business-page.png",
+        "note": "Business page"
+      },{
         "caption": "Sign out",
-        "imgref": "images/5.sign-out.png",
+        "imgref": "images/sign-out.png",
         "note": "User is asked to confirm that they want to sign out"
+      }]
+    },{
+      "title": "People and permissions",
+      "path": [{
+        "caption": "Business page",
+        "imgref": "images/business-page.png",
+        "note": "Business page"
+      },{
+        "caption": "People and permissions",
+        "imgref": "images/people-and-permissions.png"
+      },{
+        "caption": "View permissions for a person",
+        "imgref": "images/people-and-permissions-individual.png"
+      },{
+        "caption": "Change business relationship",
+        "imgref": "images/people-change-permission.png",
+        "note": "'Business partner' appears twice in the list."
+      },{
+        "caption": "Change permissions",
+        "imgref": "images/people-change-permission-2.png"
+      },{
+        "caption": "Remove yourself from business",
+        "imgref": "images/remove-self.png"
+      },{
+        "caption": "Give someone permission",
+        "imgref": "images/add-person.png"
+      },{
+        "caption": "Set business relationship",
+        "imgref": "images/add-person-relationship.png"
+      },{
+        "caption": "Set permissions",
+        "imgref": "images/add-person-permissions.png"
+      },{
+        "caption": "Find a firm",
+        "imgref": "images/add-person-by-firm.png"
+      },{
+        "caption": "Find a firm - search results",
+        "imgref": "images/add-person-by-firm-results.png"
+      },{
+        "caption": "View and select agents in firm",
+        "imgref": "images/add-person-by-firm-view-agents.png"
+      },{
+        "caption": "Set permissions for agents",
+        "imgref": "images/add-person-by-firm-set-permissions.png"
+      },{
+        "caption": "Agents added",
+        "imgref": "images/people-and-permissions-agents-added.png"
       }]
     },{
       "title": "Sign in and forgotten password",
       "path": [{
         "caption": "Sign in",
-        "imgref": "images/6.sign-in.png"
+        "imgref": "images/sign-in.png"
       },{
         "caption": "Forgotten password",
-        "imgref": "images/7.forgotten-password.png"
+        "imgref": "images/forgotten-password.png"
+      }]
+    },{
+      "title": "Land",
+      "path": [{
+        "caption": "View land parcels",
+        "imgref": "images/land-parcels.png"
+      }]
+    },{
+      "title": "Business details",
+      "path": [{
+        "caption": "View business details",
+        "imgref": "images/business-details.png"
+      },{
+        "caption": "Amend business details",
+        "imgref": "images/business-details-amend.png"
+      }]
+    },{
+      "title": "Personal details",
+      "path": [{
+        "caption": "View personal details",
+        "imgref": "images/personal-details.png"
+      },{
+        "caption": "Amend personal details",
+        "imgref": "images/personal-details-amend.png"
+      },{
+        "caption": "Confirmation",
+        "imgref": "images/personal-details-confirm.png"
+      }]
+    },{
+      "title": "Add business",
+      "path": [{
+        "caption": "Choose type of business",
+        "imgref": "images/add-business.png"
+      },{
+        "caption": "Farm - coming soon",
+        "imgref": "images/add-business-farm.png"
+      },{
+        "caption": "Firm of agents",
+        "imgref": "images/add-business-agency.png"
+      },{
+        "caption": "Done",
+        "imgref": "images/add-business-agency-done.png"
+      }]
+    },{
+      "title": "Add someone to an agency",
+      "path": [{
+        "caption": "Agency page",
+        "imgref": "images/business-page-agency.png"
+      },{
+        "caption": "People and permissions",
+        "imgref": "images/people-and-permissions-agency.png"
+      },{
+        "caption": "Add person",
+        "imgref": "images/add-person-to-agency.png"
+      },{
+        "caption": "Set relationship",
+        "imgref": "images/add-person-to-agency-relationship.png"
+      }]
+    },{
+      "title": "Other pages",
+      "path": [{
+        "caption": "Contact RPA",
+        "imgref": "images/contact-RPA.png"
       }]
     }
   ]}
