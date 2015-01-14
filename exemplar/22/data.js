ac318e05956665d8a3fa5a5b26091929ce5dbb21
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-01-14 14:24:22 +0000

    add civil claims screens

--- exemplar/22/data.js
+++ exemplar/22/data.js
@@ -1,35 +1,41 @@
 var data = {
-  "service": "22. Civil claims",
-  "last-updated": "[DATE HERE]",
+  "service": "22. Evict a tenant using accelerated possession",
+  "last-updated": "10 January 2015",
   "userjourneys": [            
     {
-      "title": "[TITLE OF SCENARIO 1]",
+      "title": "A landlord wishing to regain possession of property from tenant",
       "path": [{
-        "caption": "[CAPTION FOR SCREEN 1]",
-        "imgref": "images/[SCREEN 1 FILENAME]",
-        "note": [
-          "This is a note.",
-          "This is another note."
-        ]
+        "caption": "GOV.UK start page",
+        "imgref": "images/1-start.png"
       },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]",
-        "note": "This is a note."
+        "caption": "The full accelerated possession form",
+        "imgref": "images/2-form.png",
+        "note": "This is a long form which is divided into sections, with relevant questions being exposed as you fill in the form depending on the answers given to previous questions."
       },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]"
-      }]
-    },{
-      "title": "[TITLE OF SCENARIO 2]",
-      "path": [{
-        "caption": "[CAPTION FOR SCREEN 1]",
-        "imgref": "images/[SCREEN 1 FILENAME]"
+        "caption": "Claimants Section",
+        "imgref": "images/3-claimants.png"
       },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]"
+        "caption": "Defendants Section",
+        "imgref": "images/4-defendants.png"
       },{
-        "caption": "[CAPTION FOR SCREEN 2]",
-        "imgref": "images/[SCREEN 2 FILENAME]"
+        "caption": "Notice Section",
+        "imgref": "images/5-notice.png"
+      },{
+        "caption": "Tenancy Agreement Section",
+        "imgref": "images/6-tenancy.png"
+      },{
+        "caption": "HMO Licences Section",
+        "imgref": "images/7-licences.png",
+        "note": "If the property is a House of Multiple Occupation, the claimaint will have to fill in details of the HMO licence"
+      },{
+        "caption": "Deposit Section",
+        "imgref": "images/8-deposit.png"
+      },{
+        "caption": "Review Page",
+        "imgref": "images/9-review.png",
+        "note": "This page summarizes all the data entered, and gives the claimant an opportunity to edit the entered data before submission."
       }]
     }
   ]}
+
+
